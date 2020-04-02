f216b4d743ca3b15d8824df5454a18a45a294544
周吕华 <zhoulvhua@zhilun.com>
2020-04-02 23:39:36 +0800

    button

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Button, {ButtonType, ButtonSize} from "./components/Button/button";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Large} disabled>button</Button>
+        <hr/>
+
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Small} disabled>button</Button>        <hr/>
+
+        <Button disabled>button</Button>
+        <hr/>
+        <Button btnType={ButtonType.Link} href={'https://www.baidu.cn'}  size={ButtonSize.Large}>button</Button>        <hr/>
+
+        <Button btnType={ButtonType.Link} href={'https://www.baidu.cn'}  disabled size={ButtonSize.Large}>button</Button>
+
       </header>
     </div>
   );
